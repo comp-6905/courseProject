ef7d92d001f37f30073aaa177ed1ca4743feb8da
git <luojiawhu@163.com>
2016-11-30 16:35:15 -0330

    final

---
+++ app.js
@@ -0,0 +1,162 @@
+var express = require('express');
+var expressSession = require('express-session');
+var stylus = require('stylus');
+var nib = require('nib');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var multer          =       require('multer');
+var upload      =   multer({ dest: './public/images/userpicture/'});
+var session = require('express-session')
+// Mongo instance local passport
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+var mongo = require('mongodb');
+var monk = require('monk');
+var db = monk('localhost:27017/mcarshare');
+//defining that we are using route
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+
+var app = express();
+function compile(str, path) {
+  return stylus(str)
+      .set('filename', path)
+      .use(nib())
+}
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+var cons = require('consolidate');
+
+// view engine setup
+app.engine('html', cons.swig);
+app.engine('jade', cons.jade);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'html');
+
+
+// uncomment after placing your favicon in /public
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(expressSession({secret:'somesecrettokenhere',}));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(stylus.middleware(
+    { src: __dirname + '/public'
+      , compile: compile
+    }
+));
+app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '/public')));
+
+//express session and passport
+app.use(require('express-session')({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// passport configuration for authentication
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+mongoose.connect('mongodb://localhost/mcarshare');
+
+
+
+// db accessible to our router
+app.use(function(req,res,next){
+  req.db = db;
+  next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+//for uploading picture multer is used as below
+app.use(multer({ dest: './public/images/userpicture/',
+  rename: function (filename) {
+    return filename;
+  },
+  onFileUploadStart: function (file) {
+    console.log(file.originalname + ' is starting ...');
+  },
+  onFileUploadComplete: function (file) {
+    console.log(file.fieldname + ' uploaded to  ' + file.path)
+  }
+}));
+//GET method for picture upload page
+app.get('/picture',function(req,res){
+  res.render('setpicture', { user : req.user });
+});
+
+//POST method for saving the picture
+app.post('/api/photo',function(req,res){
+  upload(req,res,function(err) {
+    if(err) {
+      return res.end("Error uploading file.");
+    }
+    res.redirect("/home" +
+        "");
+  });
+});
+
+
+
+// catch 404 and forward to error handler
+/*app.use(function(req, res, next) {
+ var err = new Error('Not Found');
+ err.status = 404;
+ next(err);
+ });*/
+
+app.use(function(req, res, next){
+  res.status(404);
+
+  // respond with html page
+  if (req.accepts('html')) {
+    res.render('404', { url: req.url });
+    return;
+  }
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
